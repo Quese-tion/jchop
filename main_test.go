0575960e462e4e6299e401df6d8f891f7a0e5012
Jacquese <jacquesewilson@gmail.com>
2021-07-23 22:04:07 -0400

    go test -v in terminal should fail because chuckdata missing qoutation

--- main_test.go
+++ main_test.go
@@ -3,12 +3,11 @@ package main
 import (
   "encoding/json"
   "testing"
-)
-
+}
 
 
 func TestData(t *testing.T)  {
-  chuckdata:= []byte(`{"ty": "success", "value": { "id": 534, "joke": "John Doe is the ultimate mutex, all threads fear him.", "categories": ["nerdy"] } }`)
+  chuckdata:= []byte(`{type": "success", "value": { "id": 534, "joke": "John Doe is the ultimate mutex, all threads fear him.", "categories": ["nerdy"] } }`)
   chuck:=new(Chuck)
   err:=json.Unmarshal(chuckdata, &chuck)
   if err !=nil {t.Fatal(err)}
